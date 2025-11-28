fd1dbb8ffdd3b877ee530051b47db914048076c9
vita <4studywork@gmail.com>
2025-11-28 21:13:26 +0100

    initial commit

---
+++ spring-03/src/main/java/ru/otus/hw/config/AppProperties.java
@@ -0,0 +1,14 @@
+package ru.otus.hw.config;
+
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+
+@Data
+@AllArgsConstructor
+public class AppProperties implements TestConfig, TestFileNameProvider {
+
+    private int rightAnswersCountToPass;
+
+    private String testFileName;
+}
