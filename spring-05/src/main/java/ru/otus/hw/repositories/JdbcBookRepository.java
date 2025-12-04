d76d7d1a582f0d7f950f412c9fe20ac623af7551
vita <4studywork@gmail.com>
2025-12-04 14:45:22 +0100

    add implementations of BookRepository

--- spring-05/src/main/java/ru/otus/hw/repositories/JdbcBookRepository.java
+++ spring-05/src/main/java/ru/otus/hw/repositories/JdbcBookRepository.java
@@ -1,19 +1,24 @@
 package ru.otus.hw.repositories;
 
 import lombok.RequiredArgsConstructor;
+import org.springframework.dao.EmptyResultDataAccessException;
 import org.springframework.jdbc.core.RowMapper;
+import org.springframework.jdbc.core.namedparam.MapSqlParameterSource;
 import org.springframework.jdbc.core.namedparam.NamedParameterJdbcTemplate;
 import org.springframework.jdbc.support.GeneratedKeyHolder;
 import org.springframework.stereotype.Repository;
+import ru.otus.hw.exceptions.EntityNotFoundException;
 import ru.otus.hw.models.Author;
 import ru.otus.hw.models.Book;
 import ru.otus.hw.models.Genre;
 
 import java.sql.ResultSet;
 import java.sql.SQLException;
+import java.text.MessageFormat;
 import java.util.ArrayList;
 import java.util.List;
 import java.util.Map;
+import java.util.Objects;
 import java.util.Optional;
 
 @Repository
@@ -24,12 +29,43 @@ public class JdbcBookRepository implements BookRepository {
 
     @Override
     public Optional<Book> findById(long id) {
-        return Optional.of(null);
+        String query = """
+                SELECT
+                   b.id AS id,
+                   b.title AS title,
+                   a.id AS author_id,
+                   a.full_name AS author_full_name,
+                   g.id AS genre_id,
+                   g.name AS genre_name
+                FROM books b
+                JOIN authors a ON b.author_id = a.id
+                JOIN genres g  ON b.genre_id = g.id  
+                WHERE b.id = :id 
+                """;
+        try {
+            Book book = jdbcTemplate.queryForObject(query, Map.of("id", id), new BookRowMapper());
+            return Optional.of(book);
+        } catch (EmptyResultDataAccessException e) {
+            return Optional.empty();
+        }
+
     }
 
     @Override
     public List<Book> findAll() {
-        return jdbcTemplate.query("SELECT id FROM books WHERE id = :id", new BookRowMapper());
+        String query = """
+                SELECT
+                   b.id AS id,
+                   b.title AS title,
+                   a.id AS author_id,
+                   a.full_name AS author_full_name,
+                   g.id AS genre_id,
+                   g.name AS genre_name
+                FROM books b
+                JOIN authors a ON b.author_id = a.id
+                JOIN genres g  ON b.genre_id = g.id
+                """;
+        return jdbcTemplate.query(query, new BookRowMapper());
     }
 
     @Override
@@ -42,22 +78,51 @@ public class JdbcBookRepository implements BookRepository {
 
     @Override
     public void deleteById(long id) {
-        //...
+        int rows = jdbcTemplate.update(
+                "DELETE FROM books WHERE id = :id",
+                Map.of("id", id)
+        );
+
+        if (rows == 0) {
+            throw new EntityNotFoundException("Book not found: id=" + id);
+        }
     }
 
     private Book insert(Book book) {
+        MapSqlParameterSource params = new MapSqlParameterSource()
+                .addValue("title", book.getTitle())
+                .addValue("author_id", book.getAuthor().getId())
+                .addValue("genre_id", book.getGenre().getId());
+
         var keyHolder = new GeneratedKeyHolder();
 
-        //...
+        jdbcTemplate.update("""
+                INSERT INTO books (title, author_id, genre_id)
+                VALUES (:title, :author_id, :genre_id)
+                        """,
+                params, keyHolder, new String[]{"id"});
 
-        //noinspection DataFlowIssue
         book.setId(keyHolder.getKeyAs(Long.class));
         return book;
     }
 
     private Book update(Book book) {
-        //...
-        // Выбросить EntityNotFoundException если не обновлено ни одной записи в БД
+        int rows = jdbcTemplate.update("""
+                UPDATE books
+                SET title = :title, author_id = :author_id, genre_id = :genre_id
+                WHERE id = :id
+                """,
+                Map.of(
+                        "title", book.getTitle(),
+                        "author_id", book.getAuthor().getId(),
+                        "genre_id", book.getGenre().getId(),
+                        "id", book.getId()
+                )
+        );
+
+        if (rows == 0) {
+            throw new EntityNotFoundException("Book not found: id=" + book.getId());
+        }
         return book;
     }
 
--- spring-05/src/main/java/ru/otus/hw/repositories/JdbcGenreRepository.java
+++ spring-05/src/main/java/ru/otus/hw/repositories/JdbcGenreRepository.java
@@ -20,17 +20,17 @@ public class JdbcGenreRepository implements GenreRepository {
     private final NamedParameterJdbcTemplate jdbcTemplate;
     @Override
     public List<Genre> findAll() {
-        return jdbcTemplate.query("SELECT id, name FROM genres", new GnreRowMapper());
+        return jdbcTemplate.query("SELECT id, name FROM genres", new GenreRowMapper());
     }
 
     @Override
     public Optional<Genre> findById(long id) {
         Genre genre = jdbcTemplate.queryForObject("SELECT id, name FROM genres WHERE id = :id",
-                Map.of("id", id), new GnreRowMapper());
+                Map.of("id", id), new GenreRowMapper());
         return Optional.of(genre);
     }
 
-    private static class GnreRowMapper implements RowMapper<Genre> {
+    private static class GenreRowMapper implements RowMapper<Genre> {
 
         @Override
         public Genre mapRow(ResultSet rs, int i) throws SQLException {
