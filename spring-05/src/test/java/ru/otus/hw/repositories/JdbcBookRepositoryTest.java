375dfaf30da7320b58167830708834720ffcb516
vita <4studywork@gmail.com>
2025-12-04 18:29:12 +0100

    delete unnecessary imports

--- spring-05/src/test/java/ru/otus/hw/repositories/JdbcBookRepositoryTest.java
+++ spring-05/src/test/java/ru/otus/hw/repositories/JdbcBookRepositoryTest.java
@@ -1,10 +1,6 @@
 package ru.otus.hw.repositories;
 
-import org.junit.jupiter.api.BeforeEach;
-import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
-import org.junit.jupiter.params.ParameterizedTest;
-import org.junit.jupiter.params.provider.MethodSource;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.jdbc.JdbcTest;
 import org.springframework.context.annotation.Import;
@@ -14,7 +10,6 @@ import ru.otus.hw.models.Book;
 import ru.otus.hw.models.Genre;
 
 import java.util.List;
-import java.util.stream.IntStream;
 
 import static org.assertj.core.api.Assertions.assertThat;
 import static org.assertj.core.api.AssertionsForClassTypes.assertThatThrownBy;
