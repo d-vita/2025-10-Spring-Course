cf367afc2edd5c442f2e320061bbe2391c4f7308
vita <4studywork@gmail.com>
2025-12-12 16:29:15 +0100

    add Model Comment

--- spring-06/src/main/resources/schema.sql
+++ spring-06/src/main/resources/schema.sql
@@ -13,7 +13,14 @@ create table genres (
 create table books (
     id bigserial,
     title varchar(255),
-    author_id bigint references authors (id) on delete cascade,
+    author_id bigint references authors(id) on delete cascade,
     genre_id bigint references genres(id) on delete cascade,
+    primary key (id)
+);
+
+create table comments (
+    id bigserial,
+    message varchar(255),
+    book_id bigint references books(id) on delete cascade,
     primary key (id)
 );
