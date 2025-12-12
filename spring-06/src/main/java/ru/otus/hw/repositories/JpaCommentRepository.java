f613eb7dbff1d42c765ef13edccc78b192bcb71a
vita <4studywork@gmail.com>
2025-12-12 20:17:44 +0100

    add Shell commands for Comment entity

---
+++ spring-06/src/main/java/ru/otus/hw/repositories/JpaCommentRepository.java
@@ -0,0 +1,47 @@
+package ru.otus.hw.repositories;
+
+import jakarta.persistence.EntityManager;
+import jakarta.persistence.PersistenceContext;
+import lombok.RequiredArgsConstructor;
+import org.springframework.stereotype.Repository;
+import org.springframework.transaction.annotation.Transactional;
+import ru.otus.hw.dto.CommentDto;
+import ru.otus.hw.models.Comment;
+
+import java.util.List;
+import java.util.Optional;
+
+@Repository
+@RequiredArgsConstructor
+public class JpaCommentRepository implements CommentRepository {
+
+    @PersistenceContext
+    private final EntityManager em;
+    @Override
+    public Optional<Comment> findById(long id) {
+        return Optional.ofNullable(em.find(Comment.class, id));
+    }
+
+    @Override
+    public List<Comment> findAllByBookId(long bookId) {
+        return em.createQuery("select c from Comment c where c.book.id = :bookId", Comment.class)
+                .setParameter("BookId", bookId)
+                .getResultList();
+    }
+
+    @Override
+    public Comment save(Comment comment) {
+        if (comment.getId() == 0) {
+            em.persist(comment);
+            return comment;
+        }
+        return em.merge(comment);
+    }
+
+    @Override
+    public void deleteById(long id) {
+        em.createQuery("delete from Comment c where c.id = :id")
+                .setParameter("id", id)
+                .executeUpdate();
+    }
+}
