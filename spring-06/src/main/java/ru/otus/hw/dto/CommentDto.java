23977421c873a15b44e7aa7b80345ced734dda93
vita <4studywork@gmail.com>
2025-12-13 18:39:11 +0100

    fix CommentDto.java

--- spring-06/src/main/java/ru/otus/hw/dto/CommentDto.java
+++ spring-06/src/main/java/ru/otus/hw/dto/CommentDto.java
@@ -4,6 +4,6 @@ package ru.otus.hw.dto;
 public record CommentDto(Long id, String message, long bookId) {
     @Override
     public String toString() {
-        return "Id: %d, Name: %s, Book title: %s".formatted(id, message, bookId);
+        return "Id: %d, Message: %s, BookId: %d".formatted(id, message, bookId);
     }
 }
