91fc3743e47fad7ef101defbabb87df48ec0db38
vita <4studywork@gmail.com>
2025-12-30 16:53:26 +0100

    fix mistakes

--- spring-09/src/main/java/ru/otus/hw/controller/BookController.java
+++ spring-09/src/main/java/ru/otus/hw/controller/BookController.java
@@ -9,7 +9,6 @@ import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.ModelAttribute;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestParam;
 import ru.otus.hw.dto.BookFormDto;
 import ru.otus.hw.exceptions.NotFoundException;
 import ru.otus.hw.services.AuthorService;
@@ -28,20 +27,39 @@ public class BookController {
 
     @GetMapping("/")
     public String getBooks(Model model) {
-        var books = bookService.findAll();
-        model.addAttribute("books", books);
+        model.addAttribute("books", bookService.findAll());
         return "books";
+    }
+
+    @GetMapping("/add")
+    public String getAddBookPage(Model model) {
+        model.addAttribute("book", BookFormDto.empty());
+        model.addAttribute("authors", authorService.findAll());
+        model.addAttribute("genres", genreService.findAll());
+        return "add";
+    }
+
+    @PostMapping
+    public String saveBook(@Valid @ModelAttribute("book") BookFormDto bookForm,
+                           BindingResult bindingResult,
+                           Model model) {
+        if (bindingResult.hasErrors()) {
+            model.addAttribute("authors", authorService.findAll());
+            model.addAttribute("genres", genreService.findAll());
+            return "add";
+        }
+
+        var bookId = bookService.insert(bookForm.title(), bookForm.authorId(), bookForm.genreId());
+        return "redirect:/" + bookId;
     }
 
     @GetMapping("/{id}/edit")
     public String getEditBookPage(@PathVariable long id, Model model) {
         var book = bookService.findById(id).orElseThrow(NotFoundException::new);
-        var authors = authorService.findAll();
-        var genres = genreService.findAll();
 
         model.addAttribute("book", book);
-        model.addAttribute("authors", authors);
-        model.addAttribute("genres", genres);
+        model.addAttribute("authors", authorService.findAll());
+        model.addAttribute("genres", genreService.findAll());
 
         return "edit";
     }
@@ -64,19 +82,6 @@ public class BookController {
     @PostMapping("/{id}/delete")
     public String deleteBook(@PathVariable long id) {
         bookService.deleteById(id);
-        return "redirect:/";
-    }
-
-    @GetMapping("/create")
-    public String addPage(Model model) {
-        return "add";
-    }
-
-    @PostMapping("/create")
-    public String saveBook(@RequestParam String title,
-                           @RequestParam("author.id") long authorId,
-                           @RequestParam("genre.id") long genreId) {
-        bookService.insert(title, authorId, genreId);
         return "redirect:/";
     }
 }
--- spring-09/src/main/java/ru/otus/hw/dto/BookFormDto.java
+++ spring-09/src/main/java/ru/otus/hw/dto/BookFormDto.java
@@ -2,14 +2,20 @@ package ru.otus.hw.dto;
 
 import jakarta.validation.constraints.NotBlank;
 import jakarta.validation.constraints.Positive;
+import jakarta.validation.constraints.Size;
+
 
 public record BookFormDto(
         Long id,
         @NotBlank(message = "Title cannot be empty")
+        @Size(min = 2, max = 100, message = "Title should be between 2 and 100 characters")
         String title,
         @Positive(message = "Author must be selected")
         Long authorId,
         @Positive(message = "Genre must be selected")
         Long genreId
 ) {
+        public static BookFormDto empty() {
+                return new BookFormDto(null, null, null, null);
+        }
 }
