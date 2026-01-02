2f71f3866f2a9709cc3b526778be4ca4cf5e667e
vita <4studywork@gmail.com>
2026-01-02 16:19:05 +0100

    templates for hw10

---
+++ spring-10/src/main/java/ru/otus/hw/converters/GenreConverter.java
@@ -0,0 +1,17 @@
+package ru.otus.hw.converters;
+
+import org.springframework.stereotype.Component;
+import ru.otus.hw.dto.GenreDto;
+import ru.otus.hw.models.Genre;
+
+@Component
+public class GenreConverter {
+
+    public GenreDto toDto(Genre genre) {
+        return new GenreDto(
+                genre.getId(),
+                genre.getName()
+        );
+    }
+
+}
