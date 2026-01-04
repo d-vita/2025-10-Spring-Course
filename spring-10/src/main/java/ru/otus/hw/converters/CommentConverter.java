3826d05a61349bb5ebbce44fd72594e5c2ab7621
vita <4studywork@gmail.com>
2026-01-04 19:30:14 +0100

    fixed mistakes

--- spring-10/src/main/java/ru/otus/hw/converters/CommentConverter.java
+++ spring-10/src/main/java/ru/otus/hw/converters/CommentConverter.java
@@ -1,11 +1,10 @@
 package ru.otus.hw.converters;
 
-import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Component;
 import ru.otus.hw.dto.CommentDto;
 import ru.otus.hw.models.Comment;
 
-@RequiredArgsConstructor
+
 @Component
 public class CommentConverter {
 
