1a3c4c3f45555ccf0418641da949a54090687b7d
vita <4studywork@gmail.com>
2026-01-30 15:38:09 +0100

    added General ExceptionHandler

--- spring-10/src/main/java/ru/otus/hw/exceptions/GlobalExceptionHandler.java
+++ spring-10/src/main/java/ru/otus/hw/exceptions/GlobalExceptionHandler.java
@@ -1,14 +1,22 @@
 package ru.otus.hw.exceptions;
 
 import org.springframework.http.HttpStatus;
-import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.ExceptionHandler;
+import org.springframework.web.bind.annotation.ResponseStatus;
 import org.springframework.web.bind.annotation.RestControllerAdvice;
 
 @RestControllerAdvice
 public class GlobalExceptionHandler {
+
     @ExceptionHandler(EntityNotFoundException.class)
-    public ResponseEntity<String> handleNotFound(EntityNotFoundException ex) {
-        return ResponseEntity.status(HttpStatus.NOT_FOUND).body(ex.getMessage());
+    @ResponseStatus(HttpStatus.NOT_FOUND)
+    public String handleNotFound(EntityNotFoundException ex) {
+        return ex.getMessage();
+    }
+
+    @ExceptionHandler(Exception.class)
+    @ResponseStatus(HttpStatus.INTERNAL_SERVER_ERROR)
+    public String handleAll(Exception ex) {
+        return "Unexpected error occurred";
     }
 }
