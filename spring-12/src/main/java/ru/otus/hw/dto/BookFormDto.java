84478fd54ac0829b50fb1c82d3c222c3ff2e6faf
vita <4studywork@gmail.com>
2026-02-13 17:40:12 +0100

    hw12

---
+++ spring-12/src/main/java/ru/otus/hw/dto/BookFormDto.java
@@ -0,0 +1,26 @@
+package ru.otus.hw.dto;
+
+import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.Positive;
+import jakarta.validation.constraints.Size;
+import jakarta.validation.constraints.NotNull;
+
+
+public record BookFormDto(
+        Long id,
+        @NotBlank(message = "Title cannot be empty")
+        @Size(min = 2, max = 100, message = "Title should be between 2 and 100 characters")
+        String title,
+
+        @NotNull(message = "Author must be selected")
+        @Positive(message = "Author must be selected")
+        Long authorId,
+
+        @NotNull(message = "Genre must be selected")
+        @Positive(message = "Genre must be selected")
+        Long genreId
+) {
+        public static BookFormDto empty() {
+                return new BookFormDto(null, "", null, null);
+        }
+}
