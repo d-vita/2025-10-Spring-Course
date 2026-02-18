c69b03949f67b13734ba560796721567539f991c
vita <4studywork@gmail.com>
2026-02-18 20:49:13 +0100

    hw13

---
+++ spring-13/src/main/java/ru/otus/hw/configuration/SecurityConfiguration.java
@@ -0,0 +1,47 @@
+package ru.otus.hw.configuration;
+
+import lombok.RequiredArgsConstructor;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.config.annotation.web.configurers.AbstractAuthenticationFilterConfigurer;
+import org.springframework.security.config.annotation.web.configurers.AbstractHttpConfigurer;
+import org.springframework.security.config.http.SessionCreationPolicy;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+import org.springframework.security.crypto.password.PasswordEncoder;
+import org.springframework.security.web.SecurityFilterChain;
+import ru.otus.hw.services.CustomUserDetailsService;
+
+
+@RequiredArgsConstructor
+@EnableWebSecurity
+@Configuration
+public class SecurityConfiguration {
+
+    private final CustomUserDetailsService userDetailsService;
+
+    @Bean
+    public SecurityFilterChain securityFilterChain(HttpSecurity http) throws Exception {
+        http
+                .csrf(AbstractHttpConfigurer::disable)
+                .sessionManagement(session -> session.sessionCreationPolicy(SessionCreationPolicy.ALWAYS))
+                .authorizeHttpRequests(auth -> auth
+                        .requestMatchers("/api/**").authenticated()
+                        .anyRequest().permitAll()
+                )
+                .formLogin(AbstractAuthenticationFilterConfigurer::permitAll
+                )
+                .logout(logout -> logout
+                        .logoutSuccessUrl("/login")
+                )
+                .userDetailsService(userDetailsService);
+
+        return http.build();
+    }
+
+    @Bean
+    public PasswordEncoder passwordEncoder() {
+        return new BCryptPasswordEncoder();
+    }
+}
---
+++ spring-13/src/test/java/ru/otus/hw/controllers/CommentControllerTest.java
@@ -0,0 +1,59 @@
+package ru.otus.hw.controllers;
+
+import org.junit.jupiter.api.Test;
+import org.mockito.Mockito;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.web.servlet.WebMvcTest;
+import org.springframework.boot.test.mock.mockito.MockBean;
+import org.springframework.security.test.context.support.WithMockUser;
+import org.springframework.test.web.servlet.MockMvc;
+import ru.otus.hw.dto.CommentDto;
+import ru.otus.hw.services.CommentService;
+
+import java.util.List;
+
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.jsonPath;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
+
+@WebMvcTest(CommentController.class)
+public class CommentControllerTest {
+    @Autowired
+    private MockMvc mockMvc;
+
+    @MockBean
+    private CommentService commentService;
+
+    @Test
+    @WithMockUser(username = "user")
+    void shouldAllowAuthorizedAccess() throws Exception {
+
+        Mockito.when(commentService.findAllByBookId(1)).thenReturn(List.of());
+
+        mockMvc.perform(get("/api/books/1/comments"))
+                .andExpect(status().isOk());
+    }
+
+    @Test
+    void shouldRejectUnauthorizedAccess() throws Exception {
+        mockMvc.perform(get("/api/books/1/comments"))
+                .andExpect(status().isUnauthorized());
+    }
+
+    @Test
+    @WithMockUser
+    void shouldReturnCommentsForBook() throws Exception {
+        long bookId = 1L;
+        List<CommentDto> comments = List.of(
+                new CommentDto(1L, "Great book", bookId)
+        );
+        Mockito.when(commentService.findAllByBookId(bookId)).thenReturn(comments);
+
+        mockMvc.perform(get("/api/books/{bookId}/comments", bookId))
+                .andExpect(status().isOk())
+                .andExpect(jsonPath("$.size()").value(comments.size()))
+                .andExpect(jsonPath("$[0].id").value(1L))
+                .andExpect(jsonPath("$[0].message").value("Great book"))
+                .andExpect(jsonPath("$[0].bookId").value(bookId));
+    }
+}
