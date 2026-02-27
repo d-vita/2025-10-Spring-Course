189ca7e9f74f0529eab584859e897b51c4c2b3e8
vita <4studywork@gmail.com>
2026-02-27 18:51:16 +0100

    added Batch for Books

--- spring-14/src/main/java/ru/otus/hw/repositories/BookRepository.java
+++
@@ -1,7 +0,0 @@
-package ru.otus.hw.repositories;
-
-import org.springframework.data.mongodb.repository.MongoRepository;
-import ru.otus.hw.models.mongo.BookMongo;
-
-public interface BookRepository extends MongoRepository<BookMongo, String> {
-}
