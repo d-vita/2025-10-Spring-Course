d24e6fb4cb27c74293fb92f604b8d98af8683cb2
vita <4studywork@gmail.com>
2026-03-08 14:48:02 +0100

    added template for hw15

---
+++ spring-15/src/main/resources/data.sql
@@ -0,0 +1,27 @@
+insert into authors(full_name)
+values
+    ('Author_1'),
+    ('Author_2'),
+    ('Author_3');
+
+insert into genres(name)
+values
+    ('Genre_1'),
+    ('Genre_2'),
+    ('Genre_3');
+
+insert into books(title, author_id, genre_id)
+values
+    ('BookTitle_1', 1, 1),
+    ('BookTitle_2', 2, 2),
+    ('BookTitle_3', 3, 3);
+
+insert into comments(message, book_id)
+values
+    ('Comment_1', 1), ('Comment_2', 2),
+    ('Comment_3', 3), ('Comment_4', 1);
+
+insert into users(username, password, enabled)
+values
+    ('admin', '$2a$10$DOWSDmY7pN0m5zFZ3L2EWeFhZJfZpZr6ZpL5M8nJkF4Z2J1L1y', true),
+    ('user1', '$2a$10$314MEUjIUx.Rvx6rPhS4uOLXD0OnIPATWmw/h1w26uAGAbO6Imei.', true);
