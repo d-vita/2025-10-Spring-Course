c6e8d700b978b35fb950e935dc6f0ab5f0089e8f
vita <4studywork@gmail.com>
2026-03-31 17:56:49 +0200

    fixed some tests

--- spring-project/src/test/java/ru/otus/hw/controllers/TariffControllerTest.java
+++ spring-project/src/test/java/ru/otus/hw/controllers/TariffControllerTest.java
@@ -6,6 +6,8 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.web.servlet.WebMvcTest;
 import org.springframework.boot.test.mock.mockito.MockBean;
 import org.springframework.test.web.servlet.MockMvc;
+import ru.otus.hw.dto.TariffDto;
+import ru.otus.hw.services.TariffService;
 
 import java.util.List;
 
@@ -13,27 +15,27 @@ import static org.springframework.test.web.servlet.request.MockMvcRequestBuilder
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.jsonPath;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
 
-@WebMvcTest(CommentController.class)
-public class CommentControllerTest {
+@WebMvcTest(TariffController.class)
+public class TariffControllerTest {
     @Autowired
     private MockMvc mockMvc;
 
     @MockBean
-    private CommentService commentService;
+    private TariffService tariffService;
 
     @Test
-    void shouldReturnCommentsForBook() throws Exception {
-        long bookId = 1L;
-        List<CommentDto> comments = List.of(
-                new CommentDto(1L, "Great book", bookId)
+    void shouldReturnAllAuthors() throws Exception {
+        List<TariffDto> authors = List.of(
+                new TariffDto(1L, "FREE"),
+                new TariffDto(2L, "BASIC")
         );
-        Mockito.when(commentService.findAllByBookId(bookId)).thenReturn(comments);
+        Mockito.when(tariffService.findAll()).thenReturn(authors);
 
-        mockMvc.perform(get("/api/books/{bookId}/comments", bookId))
+        mockMvc.perform(get("/api/tariffs"))
                 .andExpect(status().isOk())
-                .andExpect(jsonPath("$.size()").value(comments.size()))
+                .andExpect(jsonPath("$.size()").value(authors.size()))
                 .andExpect(jsonPath("$[0].id").value(1L))
-                .andExpect(jsonPath("$[0].message").value("Great book"))
-                .andExpect(jsonPath("$[0].bookId").value(bookId));
+                .andExpect(jsonPath("$[0].name").value("FREE"))
+                .andExpect(jsonPath("$[1].name").value("BASIC"));
     }
 }
--- spring-project/src/test/java/ru/otus/hw/controllers/UserControllerTest.java
+++ spring-project/src/test/java/ru/otus/hw/controllers/UserControllerTest.java
@@ -21,46 +21,48 @@ import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
 
 @WebMvcTest(BookController.class)
-class BookControllerTest {
+class UserControllerTest {
     @Autowired
     private MockMvc mockMvc;
 
     @MockBean
-    private UserService bookService;
+    private UserService userService;
 
     @Test
-    void shouldReturnAllBooks() throws Exception {
-        List<UserDto> books = List.of(
-                new UserDto(1L, "Book 1", new TariffDto(1L, "Author 1"), new GenreDto(1L, "Fiction")),
-                new UserDto(2L, "Book 2", new TariffDto(2L, "Author 2"), new GenreDto(2L, "Science"))
+    void shouldReturnAllUsers() throws Exception {
+        List<UserDto> users = List.of(
+                new UserDto(1L, "john_doe", "john@example.com", "password_hash", new TariffDto(1L, "FREE")),
+                new UserDto(2L, "jane_doe", "jane@example.com", "password_hash", new TariffDto(2L, "BASIC"))
         );
-        Mockito.when(bookService.findAll()).thenReturn(books);
+        Mockito.when(userService.findAll()).thenReturn(users);
 
-        mockMvc.perform(get("/api/books"))
+        mockMvc.perform(get("/api/users"))
                 .andExpect(status().isOk())
-                .andExpect(jsonPath("$.size()").value(books.size()))
-                .andExpect(jsonPath("$[0].title").value("Book 1"))
-                .andExpect(jsonPath("$[0].author.fullName").value("Author 1"))
-                .andExpect(jsonPath("$[0].genre.name").value("Fiction"));
+                .andExpect(jsonPath("$.size()").value(users.size()))
+                .andExpect(jsonPath("$[0].username").value("john_doe"))
+                .andExpect(jsonPath("$[0].email").value("john@example.com"))
+                .andExpect(jsonPath("$[0].password").value("password_hash"))
+                .andExpect(jsonPath("$[0].user.tariff.name").value("FREE"));
     }
 
     @Test
-    void shouldReturnBookById() throws Exception {
-        UserDto book = new UserDto(1L, "Book 1", new TariffDto(1L, "Author 1"), new GenreDto(1L, "Fiction"));
-        Mockito.when(bookService.findById(1L)).thenReturn(book);
+    void shouldReturnUserById() throws Exception {
+        UserDto book = new UserDto(1L,"john_doe", "john@example.com", "password_hash", new TariffDto(1L, "FREE"));
+        Mockito.when(userService.findById(1L)).thenReturn(book);
 
-        mockMvc.perform(get("/api/books/{id}", 1L))
+        mockMvc.perform(get("/api/users/{id}", 1L))
                 .andExpect(status().isOk())
-                .andExpect(jsonPath("$.title").value("Book 1"))
-                .andExpect(jsonPath("$.author.fullName").value("Author 1"))
-                .andExpect(jsonPath("$.genre.name").value("Fiction"));
+                .andExpect(jsonPath("$[0].username").value("john_doe"))
+                .andExpect(jsonPath("$[0].email").value("john@example.com"))
+                .andExpect(jsonPath("$[0].password").value("password_hash"))
+                .andExpect(jsonPath("$[0].user.tariff.name").value("FREE"));
     }
 
     @Test
-    void shouldCreateBook() throws Exception {
+    void shouldCreateUser() throws Exception {
         UserFormDto form = new UserFormDto("New Book", 1L, 1L);
         UserDto created = new UserDto(1L, "New Book", new TariffDto(1L, "Author 1"), new GenreDto(1L, "Fiction"));
-        Mockito.when(bookService.insert(any(UserFormDto.class))).thenReturn(created);
+        Mockito.when(userService.insert(any(UserFormDto.class))).thenReturn(created);
 
         mockMvc.perform(post("/api/books")
                         .contentType(MediaType.APPLICATION_JSON)
@@ -72,11 +74,11 @@ class BookControllerTest {
     }
 
     @Test
-    void shouldUpdateBook() throws Exception {
-        UserFormDto form = new UserFormDto("Updated Book", 1L, 1L);
+    void shouldUpdateUser() throws Exception {
+        UserFormDto form = new UserFormDto("Updated User", 1L, 1L);
         UserDto updated = new UserDto(1L, "Updated Book", new TariffDto(1L, "Author 1"), new GenreDto(1L, "Fiction"));
 
-        Mockito.when(bookService.update(eq(1L), any(UserFormDto.class))).thenReturn(updated);
+        Mockito.when(userService.update(eq(1L), any(UserFormDto.class))).thenReturn(updated);
 
         mockMvc.perform(put("/api/books/{id}", 1L)
                         .contentType(MediaType.APPLICATION_JSON)
@@ -88,10 +90,10 @@ class BookControllerTest {
     }
 
     @Test
-    void shouldDeleteBook() throws Exception {
-        mockMvc.perform(delete("/api/books/{id}", 1L))
+    void shouldDeleteUser() throws Exception {
+        mockMvc.perform(delete("/api/users/{id}", 1L))
                 .andExpect(status().isNoContent());
 
-        Mockito.verify(bookService).deleteById(1L);
+        Mockito.verify(userService).deleteById(1L);
     }
 }
